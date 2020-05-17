f0d316360d089104735be1e06d9fcd40f8ae479f
olucheye <seyefree2rhyme@yahoo.com>
2020-05-17 16:12:18 +0100

    registration authentication built

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const shortid = require('shortid');
 const dotenv = require('dotenv').config();
 const session = require('express-session');
 const passport = require('passport');
-const passportLocal = require('passport-local-mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+//@ call session
 app.use(session({
     secret: process.env.SECRET,
-    resave: true,
-    saveUninitialized: true
+    resave: false,
+    saveUninitialized: false
 }));
 
+//@initialize passport and pass session in succession
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -25,19 +28,31 @@ app.use(passport.session());
 //@DB Credentials
 const mongodb = process.env.DB_URI;
 mongoose.connect(mongodb, {useNewUrlParser:true,  useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
 const db = mongoose.connection;
 db.on('open', ()=> console.log(`Database is now connected`));
 
-//@SCHEMA
-const User = new mongoose.Schema({
-    firstName: {type: String, required: true, trim: true},
-    lastName: {type: String, required: true, trim: true},
+//@DESC: DB SCHEMA
+const clientSchema = new mongoose.Schema({
+    id:{type: String, default: shortid.generate},
+    username: {type: String, required: true, trim: true},
+    //lastName: {type: String, required: true, trim: true},
     email: {type: String, required: true, trim: true},
     password: {type: String, required: true, trim: true},
-    address: {type: String, required: true, trim: true}
+    //address: {type: String, required: true, trim: true}
 });
 
+//@plugin localMongoose right after Schema
+clientSchema.plugin(passportLocalMongoose);
 
+//@DESC: db model
+const Client = mongoose.model("Client", clientSchema);
+
+passport.use(Client.createStrategy());
+passport.serializeUser(Client.serializeUser());
+passport.deserializeUser(Client.deserializeUser());
+
+//@DESC: Route calls
 app.get('/', (req,res)=>{
     res.render('landing');
 });
@@ -48,6 +63,27 @@ app.get('/register', (req,res)=>{
 
 app.get('/login', (req,res)=>{
     res.render('login');
+});
+
+app.get('/order', (req,res)=>{
+    
+});
+
+//@DESC: POST route
+app.post('/register', (req,res)=>{
+    const{id, username, email, password} = req.body;
+
+    Client.register({username:username}, password)
+        .then(user => {
+            passport.authenticate('local')(req,res, function(){
+                res.redirect('/order')
+            });
+        })
+        .catch(err=>{
+            res.redirect('/register'),
+            console.log(`there was an error`)
+        });
+
 });
 
 const port = (process.env.PORT || 3000);
